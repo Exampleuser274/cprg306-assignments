1e855d3d702100944eb73902eb39568f3dcb87f6
Exampleuser274 <alexanderkovach6@gmail.com>
2026-09-15 10:18:26 -0600

    finished week-2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+export default  function StudentInfo(){
+    return(
+        <main>
+            <h2>Alexander Kovach</h2>
+            <Link href="https://github.com/Exampleuser274/cprg306-assignments">Github</Link>
+        </main>
+    );
+}
