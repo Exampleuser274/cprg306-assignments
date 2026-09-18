dd9f230578377759eb89e1fc3a0d0a3907dc062b
Ian Vanderkley <ianvanderkley@gmail.com>
2026-09-18 17:08:48 -0600

    Complete week 2 assignment

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,9 +1,13 @@
-import Link from 'next/link'
-export default  function StudentInfo(){
-    return(
-        <main>
-            <h2>Alexander Kovach</h2>
-            <Link href="https://github.com/Exampleuser274/cprg306-assignments">Github</Link>
-        </main>
-    );
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Ian Vanderkley</p>
+
+      <Link href="https://github.com/Exampleuser274/cprg306-assignments/tree/Ian-Vanderkley">
+        My GitHub Repository
+      </Link>
+    </div>
+  );
 }
