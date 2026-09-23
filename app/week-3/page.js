c127b7aa5ec6e568cf15a48b609696d2b8a5eefd
jericacilo <eric.acilo@live.com>
2026-09-23 09:19:35 -0600

    item js called into page js

---
+++ app/week-3/page.js
@@ -0,0 +1,10 @@
+import Item from "./item.js";
+
+export default function Page() {
+    return(
+        <main>
+            <h2>Hello World from Item Function</h2>
+            <Item />
+        </main>
+    );
+}
