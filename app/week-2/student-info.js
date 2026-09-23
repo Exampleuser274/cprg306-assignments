7d085eda3f883bd7f9ccbc61953030a84e3856bd
jericacilo <eric.acilo@live.com>
2026-09-23 08:55:03 -0600

    github link update

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,7 +3,7 @@ export default  function StudentInfo(){
     return(
         <main>
             <h2>John Eric Acilo</h2>
-            <Link href="https://github.com/Exampleuser274/cprg306-assignments">Github</Link>
+            <Link href="https://github.com/eAxqui">Github</Link>
         </main>
     );
 }
