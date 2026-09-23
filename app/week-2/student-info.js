9540cd02273cbe18a20fc2492c1b071afba94fab
jericacilo <eric.acilo@live.com>
2026-09-23 08:53:00 -0600

    student name typog update

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 export default  function StudentInfo(){
     return(
         <main>
-            <h2>Alexander Kovach</h2>
+            <h2>John Eric Acilo</h2>
             <Link href="https://github.com/Exampleuser274/cprg306-assignments">Github</Link>
         </main>
     );
